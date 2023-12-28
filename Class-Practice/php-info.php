6815752839a5f3fff03c57c03fcdec427bdf8475
ergophile-geek <hawanatamang@gmail.com>
2023-12-28 21:49:23 +0545

    code: Add php info code

---
+++ Class-Practice/php-info.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - Information</title>
+</head>
+<body>
+    <h2 style="text-align: center;">Information of PHP in my OS </h2>
+    <?php
+    phpinfo();
+    ?>
+</body>
+</html>
